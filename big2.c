9b3c7ca516684c152052b61ad6acef965f46af36
abc <abc@gamil.com>
2025-01-27 07:13:40 +0000

    cptoject

---
+++ big2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int b1() {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2) {
+        printf("Largest number is %d.", num1);
+    } else {
+        printf("Largest number is %d.", num2);
+    }
+
+    return 0;
+}
